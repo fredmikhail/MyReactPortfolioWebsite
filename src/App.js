7267a62f7ccb4e5559f28f5c36fa52ba835b603f
Frederic Mikhail <fred.ekladioce@gmail.com>
2023-01-20 22:47:42 -0500

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
-import Services from './components/services/Services';
+// import Services from './components/services/Services';
 import Qualifications from './components/qualifications/Qualifications';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
